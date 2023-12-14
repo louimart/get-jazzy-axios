300347f6359c70e2379eb85f6da8f45922545f93
Louis Martin <louimart@gmail.com>
2023-12-14 17:30:58 -0600

    form created in html. In Progress

--- server/server.js
+++ server/server.js
@@ -53,6 +53,11 @@ app.get('/artist', (req, res) => {
 
 // TODO - Add GET for songs
 
+app.get('/song', (req, res) => {
+    res.send(songListArray);
+});
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
+
